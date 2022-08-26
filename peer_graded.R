0b0bb8539ba53058289caace5d6881db73fae5a3
S Lee <sanlee8495@gmail.com>
2022-08-26 11:37:40 -0700

    8/26/22

--- peer_graded.R
+++ peer_graded.R
@@ -5,7 +5,7 @@ library(DT)
 
 # reference code: Pages.R
 
-#####Import Data
+##### Import Data
 
 dat<-read_csv(url("https://www.dropbox.com/s/uhfstf6g36ghxwp/cces_sample_coursera.csv?raw=1"))
 dat<- dat %>% select(c("pid7","ideo5","newsint","gender","educ","CC18_308a","region"))
@@ -15,7 +15,12 @@ dat %>%
   dplyr::group_by(ideo5) %>% 
   dplyr::count(pid7) -> dat_2
 
-#####Make your app
+
+dat %>% 
+  dplyr::group_by(ideo5) %>% 
+  dplyr::count(CC18_308a) -> dat_3
+
+##### Make your app
 
 ui <- 
   ui <- navbarPage(
@@ -32,12 +37,12 @@ ui <-
                width = 500)),
              mainPanel(
                tabsetPanel(
-                 id = "tab1",
+                 id = "Tab1",
                  tabPanel(
                    title = "tab1",
              plotOutput("plot1")),
              tabPanel(
-               title = "tab2",
+               title = "Tab2",
                plotOutput("plot2")
              ))
     )),
@@ -57,6 +62,17 @@ server<-function(input,output){
         ggplot2::coord_cartesian(ylim = c(0, 100), xlim = c(0, 8)) +
         ggplot2::labs(x = "7 Point Party ID, 1 = Very D, 7 = Very R",
                       y = "Count")
+    })
+    
+    output$plot2 <- renderPlot({
+      
+      plot_dat_2 <- dplyr::filter(dat_3, ideo5 == input$slider)
+      
+      ggplot2::ggplot(plot_dat_2, mapping = aes(x = CC18_308a, y = n)) +
+        ggplot2::geom_bar(stat = "identity") +
+        ggplot2::coord_cartesian(xlim = c(0, 5)) +
+        ggplot2::labs(x = "Trump Support",
+                      y = "count")
     })
 
     
